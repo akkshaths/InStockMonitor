6d143b4e972c0c99fc7ea3466a3aae446d15b3e9
akkshaths <63518142+akkshaths@users.noreply.github.com>
2020-06-17 23:40:47 -0400

    Create AmazonInStock.py

---
+++ AmazonInStock.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 00:37:04 2020
+
+@author: akkie
+"""
+
+
+from bs4 import BeautifulSoup
+import googlesearch as goog
+import requests
+import html5lib
+import pandas as pd
+import csv
+
